53ad79483823b19326401dcce0475c9433aac6e2
Charles Dierbach <cdierbach@towson.edu>
2024-02-19 17:47:48 -0500

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,14 @@
+public class Temperature {
+  private double temperature;
+
+  // constructor
+  public Temperature(double t) {
+    temperature = t;
+  }
+
+  // getter method named getCelsius
+  
+
+  // getter method named getFahrenheit
+  
+}
